3a7b67495a669fd9072c8d38fb00e13e8674af9e
Pyr0 <jordanrandles@googlemail.com>
2020-05-03 19:41:35 +0100

    Set open ip

--- config.js
+++ config.js
@@ -2,7 +2,8 @@
 {
         "service" : {
                 "api" : "http",
-                "port" : 8080
+                "port" : 8080,
+				"ip" : "0.0.0.0"
         },
         "http" : {
                 "script" : "/"
