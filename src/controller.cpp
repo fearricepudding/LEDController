06bc9ca0e00461435fbd39b62f61f900b41a2937
fearricepudding <jordanrandles@googlemail.com>
2020-08-23 21:25:00 +0100

    Working

---
+++ src/controller.cpp
@@ -0,0 +1,19 @@
+#include "ws2812-rpi.h"
+
+#define PIXELS 191
+
+int main(int argc, char **argv){
+    NeoPixel *n=new NeoPixel(PIXELS);
+//    int i = 0;
+//    n->setBrightness(.5f);
+//    for(i=0;i < PIXELS; i++){
+//
+//	    n->setPixelColor(i, 100, 150, 100);
+//    }
+//    n->show();
+	while(true){
+    	n->rainbowCycle(5);
+	}
+    delete n;
+    return 0;
+}
--- src/libs.h
+++
@@ -1,9 +0,0 @@
-#include <cppcms/application.h>
-#include <cppcms/service.h>
-#include <cppcms/http_response.h>
-#include <cppcms/http_request.h>
-#include <cppcms/url_dispatcher.h>
-#include <cppcms/url_mapper.h>
-#include <cppcms/applications_pool.h>
-#include <iostream>
-#include <stdlib.h>
